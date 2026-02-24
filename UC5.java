b54e8bc04435b5b2f16c44de4d9d86da48ddb67b
AnushkaBag-22 <anushkabag07@gmail.com>
2026-02-24 09:20:42 +0530

    UC5 done

---
+++ UC5.java
@@ -0,0 +1,34 @@
+public class OOPSBannerUC5 {
+    public static void main(String[] args) {
+
+        // Inline array declaration and initialization
+        String[] banner = {
+            String.join(" ",
+                " ******** ", " ******  ", " ******  ", " ******** "
+            ),
+            String.join(" ",
+                "*        *", "*      * ", "*      * ", "*        *"
+            ),
+            String.join(" ",
+                "*        *", "*      * ", "*      * ", "*        *"
+            ),
+            String.join(" ",
+                "*        *", " ******  ", " ******  ", " ******** "
+            ),
+            String.join(" ",
+                "*        *", "*        ", "*        ", "*     *  "
+            ),
+            String.join(" ",
+                "*        *", "*        ", "*        ", "*      * "
+            ),
+            String.join(" ",
+                " ******** ", "*        ", "*        ", " ******  "
+            )
+        };
+
+        // Enhanced for loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
